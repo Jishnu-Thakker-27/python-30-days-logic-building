5d602a5315da9e572f75fdde2f02f762bff733d8
Jishnu Thakker <jishnuthakker2007@gmail.com>
2026-03-14 23:54:37 +0530

    Updated Loop For Asking Number.

--- 01_Even_Odd_Checker.py
+++ 01_Even_Odd_Checker.py
@@ -1,6 +1,8 @@
-a=int(input("Enter The Number:- "))
-
-if a%2==0:
-    print("Number Is Even.")
-else:
-    print("Number Is Odd.")
+while True:
+    a=int(input("Enter The Number:- "))
+    if a%2==0:
+        print("Number Is Even.")
+        break
+    else:
+        print("Number Is Odd.")
+        break
