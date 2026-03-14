225ed610e8ae7f2f23432aa0286a25c5135907cb
Jishnu Thakker <jishnuthakker2007@gmail.com>
2026-03-14 23:58:52 +0530

    Update 02_Prime_Number_Checker.py

--- 02_Prime_Number_Checker.py
+++ 02_Prime_Number_Checker.py
@@ -1,8 +1,13 @@
-a=int(input("Enter Your Nunber:- "))
+while True:
+    a=int(input("Enter Your Nunber:- "))
 
-for i in range(2,a):
-    if(a%i==0):
-        print("It Is A Composite Number.")
+    for i in range(2,a):
+        if(a%i==0):
+            print("It Is A Composite Number.")
+            break
+        if(a==1):
+            print("It is neither prime not composite")
+
+    else:
+        print("It Is A Prime Number.")
         break
-else:
-    print("It Is A Prime Number.")
