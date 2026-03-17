9ffd63eefe430e2cc048c498acf6bf7d0643aa04
Jishnu Thakker <jishnuthakker2007@gmail.com>
2026-03-17 10:27:18 +0530

    Updated prime number checker
    Add prime number checker with user input handling.

--- Day 02/Prime_Number_Checker.py
+++ Day 02/Prime_Number_Checker.py
@@ -1,1 +1,13 @@
+while True:
+    a=int(input("Enter Your Nunber:- "))
 
+    for i in range(2,a):
+        if(a%i==0):
+            print("It Is A Composite Number.")
+            break
+        if(a==1):
+            print("It is neither prime not composite")
+
+    else:
+        print("It Is A Prime Number.")
+        break
