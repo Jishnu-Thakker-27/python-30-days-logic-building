1032166d94e8dfb681700911e11ea8c58d9cfcee
Jishnu Thakker <jishnuthakker2007@gmail.com>
2026-03-24 22:58:35 +0530

    Delete 05_Multiplication_Table.py

--- 05_Multiplication_Table.py
+++
@@ -1,6 +0,0 @@
-while True:
-    number=int(input("Enter The Number."))
-    print(f"The Multiplication Table of {number} is:-")
-    for i in range (1,11):
-        print(f"{number}x{i}={number*i}")
-    continue
