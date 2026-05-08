7dae5c08ff1325473e72216dab0e1a4da5a326c7
Jishnu Thakker <jishnuthakker2007@gmail.com>
2026-05-08 23:15:27 +0530

    Refactor login and signin functions for clarity and adding the excpetiong handling to the program

--- 20. Login System With File Storage/login.py
+++ 20. Login System With File Storage/login.py
@@ -1,53 +1,94 @@
-def signin():
-    name = input("Enter Your Name: ")
-    password = input("Enter your password: ")
-    confirm_password = input("Confirm your password: ")
+def login():
+    username = input("Enter the username: ")
+    password = input("Enter the password: ")
 
-    if password != confirm_password:
-        print("Invalid Password Confirmation")
+    try:
+        with open("login.txt", "r") as f:
+            data = f.readlines()
+    except FileNotFoundError:
+        print("No account found. Please sign in first.")
         return
 
-    with open("login.txt", "r") as f:
-        data = f.readlines()
+    user_found = False
 
-    for line in data:
-        stored_name, stored_password = line.strip().split("\t")
+    for user in data:
+        try:
+            existing_user, existing_password = user.strip().split(":")
+        except ValueError:
+            continue
 
-        if name == stored_name:
-            print("User already exists! Please login.")
-            return
+        if existing_password == password and existing_user == username:
+            user_found = True
+            break
 
-    with open("login.txt", "a") as f:
-        f.write(f"{name}\t{password}\n")
+    if user_found:
+        print("You have been successfully logged in.")
+    else:
+        print("Invalid Username or Password")
 
-    print("Signup successful!")
+
+def signin():
+    special = "!@#$%^&*()_+-"
+
+    username = input("Enter Unique Username: ")
+    password = input("Enter The Password containing any of these special characters (!@#$%^&*()_+-): ")
+
+    try:
+        with open("login.txt", "r") as f:
+            data = f.readlines()
+    except FileNotFoundError:
+        data = []
+
+    user_found = False
+
+    for user in data:
+        try:
+            existing_user, existing_password = user.strip().split(":")
+        except ValueError:
+            continue
+
+        if existing_user == username:    #password can be same but we ask a user to give unique username while first time sign up.
+            user_found = True
+            break
+
+    if user_found:
+        print(f"{username} username already exists. Try to login or enter a unique username.")
+        return signin()
+    
+    elif username == "" or password == "":
+        print("Username and password cannot be empty.")
+        return signin()
+    
+    elif ":" in username or ":" in password:  #while saving in file i save it as username:password so : will be in username or password it would cause confusion while spliting.
+        print("Username and password cannot contain ':'.")
+        return signin()
 
 
-def login():
-    name = input("Enter Your Name: ")
-    password = input("Enter your password: ")
 
-    with open("login.txt", "r") as f:
-        data = f.readlines()
+    elif any(char in special for char in password):
+        with open("login.txt", "a") as f:
+            f.write(f"{username}:{password}\n")
 
-    for line in data:
-        stored_name, stored_password = line.strip().split("\t")
+        print("Sign In Done Successfully.")
+    else:
+        print("Password must contain at least one special character.")
+        return signin()
 
-        if name == stored_name and password == stored_password:
-            print("Login successful!")
-            return
 
-    print("Invalid Login credentials")
-
-print("Welcome to the website.")
+print("-----Welcome!-----")
 print("1. Login")
-print("2. Sign Up")
+print("2. Signin")
 
-user = int(input("Enter 1 for Login and 2 for SignUp: "))
+while True:
+    try:
+        action = int(input("Enter 1 for Login and 2 for Signin: "))
+    except ValueError:
+        print("Please enter only a number.")
+        continue
 
-if user == 1:
-    login()
-elif user == 2:
-    signin()
-else:
-    print("Invalid choice")
+    if action == 1:
+        login()
+    elif action == 2:
+        signin()
+    else:
+        print("Invalid Input.")
