6628154ff7a2fd9aacc01e0570b313a5f8fcaf58
Jishnu Thakker <jishnuthakker2007@gmail.com>
2026-05-13 01:38:36 +0530

    Implement CSV analysis for missing values and duplicates
    Added functionality to read CSV file and analyze missing values and duplicates.

--- 22.  CSV Data Analyzer/CSV_Analyzer.py
+++ 22.  CSV Data Analyzer/CSV_Analyzer.py
@@ -1,4 +1,22 @@
-# Work In Progress!!!
+print("CSV Analyzer")
+
+with open("file.csv") as f:
+    data=f.readlines()
+print(data)
+rows=len(data)-1
+columns=len(data[0].strip().split(","))
+
+print("Rows: ",rows)
+print("Columns: ",columns)
+
+total_count=0
+
+for row in data[1:]:
+    row_item=row.strip().split(",")
+    for empty in row_item:
+        if len(empty.strip())==0:
+            total_count+=1
+
 
 count_name=0
 count_department=0
@@ -22,31 +40,41 @@ for row in data:
             elif index==4:
                 count_city+=1
                 
-print("Missing indexs: \n")
-print("Total: ",total_count)
-print()
+print("Missing values: ")
 if count_name!=0:
-    print("Name: ",count_name)
-    print()
+    print(" Name: ",count_name)
+    
 else:
     pass
 if count_roll!=0:
-    print("Roll Number: ",count_roll)
-    print()
+    print(" Roll Number: ",count_roll)
+    
 else:
     pass
 if count_department!=0:
-    print("Department: ",count_department)
-    print()
+    print(" Department: ",count_department)
+
 else:
     pass
 if count_batch!=0:
-    print("Batch: ",count_batch)
-    print()
+    print(" Batch: ",count_batch)
+
 else:
     pass
 if count_city!=0:
-    print("City: ",count_city)
-    print()
+    print(" City: ",count_city)
+
 else:
     pass
+
+print(" Total: ",total_count)
+
+        
+e=set()
+
+for info in data[1:]:
+    e.add(info.strip())
+    
+    
+duplicate_rows=len(data[1:])-len(e)
+print("Duplicate Rows: ",duplicate_rows)
