83b337eb65156f8382838c1eb77b9c659a6c8577
Jishnu-Thakker-27 <jishnuthakker2007@gmail.com>
2026-05-25 21:29:57 +0530

    Add view and delete functionality

--- 23 Modular Task Manager/task_operations.py
+++ 23 Modular Task Manager/task_operations.py
@@ -1,20 +1,50 @@
 tasks=[]
-task_items={}
 def add_task():
+    task_items={}
     task_id=int(input("Enter the task number "))
+    for item in tasks:
+        if item["id"]==task_id:
+            print("Task Id already exist.")
+            return add_task()
+        
     task=input("Enter the task") 
-    status=input("Add status to the task([Y] for Complete/[N] for Incomplete) ")
-    task_items["id"]=task_id
-    task_items["Task"]=task
-    task_items["Status"]=status
+    status=input("Add status to the task([Y] for Complete/[N] for Incomplete) ").upper()
+    task_items['id']=task_id
+    task_items['Task']=task
+    task_items['Status']=status
     
     tasks.append(task_items)
     print(tasks)
 def delete_task():
-    pass
+    delete_id=int(input("Enter the id of task you want to delete."))
+    for index,item in enumerate(tasks):
+        if item['id']==delete_id:
+            tasks.pop(index)
+            print("Task deleted successfully.")
+            break
+    else:
+        print("No such task id exists.")
+            
 def complete_task():
-    pass
+    complete_id=int(input("Enter the id of task you have completed."))
+    for index,item in enumerate(tasks):
+        if item['id']==complete_id:
+            if item['Status']=="Y":
+                print("This task was already marked as completed")
+            else:
+                item['Status']="Y"
+                print("Task Marked as completed")
+            break
+    else:
+        print("No such task id exists.")
 def save_task():
     pass
 def view_task():
-    pass
+    if not tasks:   
+        print("No tasks found.")
+        return
+    for item  in tasks:
+        print(f"{item['id']}")
+        print(f"{item['Task']}")
+        print(f"{item['Status']}")
+    print("These are your Current Tasks.")
