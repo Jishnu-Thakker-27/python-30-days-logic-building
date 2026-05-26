1e8769649b31ed808db57aa9df0a3f75d0dd6d88
Jishnu-Thakker-27 <jishnuthakker2007@gmail.com>
2026-05-26 23:21:47 +0530

    Add main menu and task manager workflow

--- 23 Modular Task Manager/main.py
+++ 23 Modular Task Manager/main.py
@@ -1,33 +1,34 @@
 from task_operations import add_task
 from task_operations import delete_task
 from task_operations import complete_task
-from task_operations import save_task
-from task_operations import view_task
+from storage import load_task
+from display import view_task
+
+
+tasks=load_task()
 
 while True:
     print("Task Manager")
     print("1.Add Task")
     print("2.View Task")
     print("3.Mark tasks as completed")
-    print("4.Save tasks permanently")
-    print("5.Delete Task")
-    print("6.Exit")
+    print("4.Delete Task")
+    print("5.Exit")
     try:
         Task=int(input("Enter the index number for your task."))
     except ValueError:
         print("Enter the integer value")
         print("Invalid Input.")
+        continue
     if Task==1:
-        add_task()
+        add_task(tasks)
     elif Task==2:
-        view_task()
+        view_task(tasks)
     elif Task==3:
-        complete_task()
+        complete_task(tasks)
     elif Task==4:
-        save_task()
+        delete_task(tasks)
     elif Task==5:
-        delete_task()
-    elif Task==6:
         break
     else:
         print("Invalid Input.")
