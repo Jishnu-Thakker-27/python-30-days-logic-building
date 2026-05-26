0949d257fbc36364a4bff7da0a72b9ec7e53bfa6
Jishnu-Thakker-27 <jishnuthakker2007@gmail.com>
2026-05-26 23:22:43 +0530

    Implement add, delete and complete task operations

--- 23 Modular Task Manager/task_operations.py
+++ 23 Modular Task Manager/task_operations.py
@@ -1,31 +1,39 @@
-tasks=[]
-def add_task():
+from storage import save_task
+
+def add_task(tasks):
     task_items={}
     task_id=int(input("Enter the task number "))
     for item in tasks:
         if item["id"]==task_id:
             print("Task Id already exist.")
-            return add_task()
+            return add_task(tasks)
         
     task=input("Enter the task") 
     status=input("Add status to the task([Y] for Complete/[N] for Incomplete) ").upper()
+    status_dict={
+        "Y": "Completed",
+        "N": "Pending",
+        
+    }
     task_items['id']=task_id
     task_items['Task']=task
-    task_items['Status']=status
+    task_items['Status']=status_dict[status]
     
     tasks.append(task_items)
-    print(tasks)
-def delete_task():
+    save_task(tasks)
+
+def delete_task(tasks):
     delete_id=int(input("Enter the id of task you want to delete."))
     for index,item in enumerate(tasks):
         if item['id']==delete_id:
             tasks.pop(index)
+            save_task(tasks)
             print("Task deleted successfully.")
             break
     else:
         print("No such task id exists.")
             
-def complete_task():
+def complete_task(tasks):
     complete_id=int(input("Enter the id of task you have completed."))
     for index,item in enumerate(tasks):
         if item['id']==complete_id:
@@ -33,18 +41,8 @@ def complete_task():
                 print("This task was already marked as completed")
             else:
                 item['Status']="Y"
+                save_task(tasks)
                 print("Task Marked as completed")
             break
     else:
         print("No such task id exists.")
-def save_task():
-    pass
-def view_task():
-    if not tasks:   
-        print("No tasks found.")
-        return
-    for item  in tasks:
-        print(f"{item['id']}")
-        print(f"{item['Task']}")
-        print(f"{item['Status']}")
-    print("These are your Current Tasks.")
