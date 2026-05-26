828ef76bef45e3f0a92c20593cd73a311c3b50c1
Jishnu-Thakker-27 <jishnuthakker2007@gmail.com>
2026-05-26 23:23:22 +0530

    Implement task loading and auto-save features

--- 23 Modular Task Manager/storage.py
+++ 23 Modular Task Manager/storage.py
@@ -1,6 +1,10 @@
-def storage():
-    task=[
-        {
-            id: 
-        }
-    ]
+import json
+
+def load_task():
+    with open("tasks.json") as f:
+        data=json.load(f)
+    return data
+
+def save_task(tasks):
+    with open("tasks.json","w") as f:
+        json.dump(tasks,f)
